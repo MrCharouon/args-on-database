dae0ae0a5df070385e9dc597aa0793b9cd05f2a1
MrCharouon <ali.khatami2009@gmail.com>
2022-08-25 23:12:36 +0430

    Separating and cleaning the program with different classes and program extensions

--- directory.py
+++ directory.py
@@ -1,8 +1,9 @@
 import os
 path = (os.path.expanduser('~/.tsewl/'))
-try:
-    os.chdir(path)
-    # print("Current working directory: {0}".format(os.getcwd()))
-except FileNotFoundError:
-    # print("Directory: {0} does not exist".format(path))
-    os.mkdir(path)
+class directorycheck():
+    try:
+        os.chdir(path)
+        # print("Current working directory: {0}".format(os.getcwd()))
+    except FileNotFoundError:
+        # print("Directory: {0} does not exist".format(path))
+        os.mkdir(path)
--- main.py
+++ main.py
@@ -1,59 +1,17 @@
 import argparse
-import sqlite3
+from directory import *
+from db_connection import *
 
-conn = sqlite3.connect('data.db')
-cur = conn.cursor()
-cur.execute("""CREATE TABLE IF NOT EXISTS invests (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_invests Text );""")
-conn.commit()
-
-list_invests = []
-list_id = []
-sql_select_Query = "select * from invests"
-cursor = conn.cursor()
-cursor.execute(sql_select_Query)
-records = cursor.fetchall()
-for row in records:
-    list_id.append(row[0])
-    list_invests.append(row[1])
-
-def INSERT_DATA(new_invest, db_ids):
-    if len(list_id) == 0:
-        number_id = 1
-    else:
-        number_id = int(list_id[-1])+1
-    query = f'INSERT INTO invests VALUES ("{number_id}", "{new_invest}")'
-    cur.execute(query)
-    conn.commit()
-
-def DELETE_DATA(delete_invest):
-    query = f'DELETE FROM invests WHERE user_invests = "{delete_invest}"'
-    cur.execute(query)
-    conn.commit()
-
-class update_db():
-    def update(slef):
-        update_invest = []
-        sql_select_Query = "select * from invests"
-        cursor = conn.cursor()
-        cursor.execute(sql_select_Query)
-        records = cursor.fetchall()
-        for row in records:
-            update_invest.append(row[1])
-        conn.close()
-        return (update_invest)
+directorycheck()
 
 parser = argparse.ArgumentParser(description="The following is a help document")
 parser.add_argument('--add', action='store', dest='a_invest', help='add a new invest to datebase')
 parser.add_argument('--remove', action='store', dest='r_invest', help='remove a invest from your watchlist')
 
-
-
 args = parser.parse_args()
 invest = (args.a_invest)
 remove = (args.r_invest)
-if (invest != None):
+if(invest != None):
     def check(w, list):
         if w in list:
             print("The investment is in the database!")
